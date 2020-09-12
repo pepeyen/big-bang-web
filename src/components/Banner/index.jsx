665f11a9073665f21a392778f09b2c662a10dcfd
pepeyen <erickfcaldeira@gmail.com>
2020-09-12 18:02:12 -0300

    Introduced Banner component

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Banner(props){
+    return(
+        <div className="page__banner --central --first-post --thin-borders">
+            <div className="page__banner-headline --flex-end-self">
+                <p className="page__banner-title --centralized-text --dark-text">
+                    {props.title}
+                </p>
+                <p className="page__banner-text --centralized-text --grey-text">
+                    {props.text}
+                </p>
+                {props.children}
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
