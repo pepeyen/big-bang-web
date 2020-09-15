22ad192b48919b702eb287a253b708f0c1a625a2
pepeyen <erickfcaldeira@gmail.com>
2020-09-15 19:32:14 -0300

    Fixed Post title positioning

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Post(props){
     return(
         <article className={props.title !== undefined ? "page__post --central --top-thin-borders --spaced-top" : "page__post --central"}>
-            {props.title !== undefined ? <header><span className="page__post-title --dark-text">{props.title}</span></header> : ''}
+            {props.title !== undefined ? <span className="page__post-title --dark-text">{props.title}</span> : ''}
             {props.children}
         </article>
     );
