e340ea98472755aeee284093877c03910e56b1af
pepeyen <erickfcaldeira@gmail.com>
2020-10-02 16:17:12 -0300

    Prep mock up data for future updates

--- src/services/mockData.js
+++ src/services/mockData.js
@@ -7,46 +7,57 @@ export const banners = [
 ]
 export const posts = [
     {
-        bannerURL: require('../assets/images/post/bg-post2.jpg'),
-        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        info: ["VIAGEM","por Fulano de Tal"]
-    },
-    {
         bannerURL: require('../assets/images/post/bg-post3.jpg'),
         title: "Nome do episódio do podcast lorem ipsum dolor sit amet consectetur eli...",
-        info: ["Namahcast #122"]
+        info: ["Namahcast #122"],
+        ID: 11
+    },
+    {
+        bannerURL: require('../assets/images/post/bg-post2.jpg'),
+        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        info: ["VIAGEM","por Fulano de Tal"],
+        ID: 681
+    },
+    {
+        bannerURL: require('../assets/images/product/bg-product1.jpg'), 
+        title: "Condicionador Namah",
+        info: ["COMPRE AGORA"],
+        ID: 404
     },
     {
         bannerURL: require('../assets/images/post/bg-post4.jpg'),
         title: "Imersão Vinyasa Flow",
-        info: ["CURSO","04 ABR"]
-    },
-    {
-        bannerURL: require('../assets/images/product/bg-product1.jpg'),
-        title: "Condicionador Namah",
-        info: ["COMPRE AGORA"]
+        info: ["CURSO","04 ABR"],
+        ID: 12023452
     }
 ];
 export const products = [
     {
-        id: 0,
         bannerURL: require('../assets/images/product/product1.jpg'),
         title: 'Livro Acenda a Sua Luz',
         info: 'Carol Rache',
-        price: 54.00
+        price: 54.00,
+        ID: 39
     },
     {
-        id: 1,
         bannerURL: require('../assets/images/product/product2.jpg'),
         title: 'Camiseta Solidária',
         info: 'Tamanho único',
-        price: 149.00
+        price: 149.00,
+        ID: 62
     },
     {
-        id: 2,
         bannerURL: require('../assets/images/product/product3.jpg'),
         title: 'Tapete Viagem Pássaro',
         info: '',
-        price: 237.00
+        price: 237.00,
+        ID: 72
+    },
+    {
+        bannerURL: require('../assets/images/product/bg-product1.jpg'), 
+        title: "Condicionador Namah",
+        info: '300 ml',
+        price: 120.00,
+        ID: 404
     }
 ];
