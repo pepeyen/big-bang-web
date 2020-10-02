b9f37418747af727530445580f3992603b88e32d
pepeyen <erickfcaldeira@gmail.com>
2020-10-02 16:18:22 -0300

    Updated multi-page query support

--- src/services/getCurrentPage.js
+++ src/services/getCurrentPage.js
@@ -1,5 +1,20 @@
-export default function getCurrentPage(){
-    let currentPageURI = window.location.hash.substr(2);
+export function getCurrentPage(){
+    const currentPageURI = window.location.hash.substr(2);
     
     return currentPageURI;
 }
+
+export function getCurrentPageID(){
+    let regex = new RegExp('[\\?&]id=([^&#]*)');
+    let results = regex.exec(window.location.hash);
+    
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+/*
+export function getCurrentPageType(){
+    let regex = new RegExp('[\\?&]type=([^&#]*)');
+    let results = regex.exec(window.location.hash);
+    
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+*/
