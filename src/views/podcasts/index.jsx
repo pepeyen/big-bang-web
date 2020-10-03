46f823d6d86b60c5f9c004f28367a66acbf7f670
pepeyen <erickfcaldeira@gmail.com>
2020-10-03 16:13:29 -0300

    Introduced podcast listing page

--- src/views/podcasts/index.jsx
+++ src/views/podcasts/index.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 //Component
 import Navbar from '../../components/Navbar';
+import Post from '../../components/Post';
+import Redirector from '../../components/Redirector';
+import RedirectorInfo from '../../components/Redirector/redirectorInfo';
+import RedirectorText from '../../components/Redirector/redirectorText';
+import RedirectorBanner from '../../components/Redirector/redirectorBanner';
 
 //Services
 import {posts} from '../../services/mockData';
@@ -18,18 +23,37 @@ function Podcasts(){
             <main>
                 <section className="page">
                     <p className="page__place-holder">Podcasts</p>
-                    {postsByType.map((element,index) => {
-                        return(
-                            <React.Fragment key={index}>
-                                <p className="">{posts[element.ID].title}</p>
-                                <p className="">{posts[element.ID].info.userNickname} #{posts[element.ID].info.userId}</p>
-                                <img 
-                                    className=""
-                                    src={posts[element.ID].bannerURL} alt={posts[element.ID].title} 
-                                />
-                            </React.Fragment>
-                        );
-                    })}
+                    <Post>
+                        {postsByType.map((element,index) => {
+                            return(
+                                <Redirector 
+                                    key={index}
+                                    redirectorType={posts[element.ID].type}
+                                    redirectorID={posts[element.ID].ID}
+                                >
+                                    <RedirectorInfo>
+                                        <RedirectorText
+                                            infoType="title"
+                                            color="dark"
+                                        >
+                                            {posts[element.ID].title}
+                                        </RedirectorText>
+                                        <RedirectorText
+                                            infoType="user"
+                                            color="grey"
+                                        >
+                                            {posts[element.ID].info.userNickname} #{posts[element.ID].info.userId}
+                                        </RedirectorText>  
+                                    </RedirectorInfo>
+                                    <RedirectorBanner
+                                        url={posts[element.ID].bannerURL}
+                                        alt={posts[element.ID].title}
+                                        type='podcast'
+                                    />
+                                </Redirector >
+                            );
+                        })}
+                    </Post>
                 </section>
             </main>
         </React.Fragment>
