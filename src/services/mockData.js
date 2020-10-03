79dd49ff54d6db06f8b7c41d1ebb9d5900177110
pepeyen <erickfcaldeira@gmail.com>
2020-10-02 23:56:47 -0300

    Minor mock API update

--- src/services/mockData.js
+++ src/services/mockData.js
@@ -1,21 +1,61 @@
+import filterByID from '../services/filterByID';
+
 export const banners = [
     {
         bannerURL: require('../assets/images/post/bg-post1.jpg'),
         title: "Lorem ipsum dolor sit amet",
         info: "Lorem ipsum dolor sit amet"
     }
-]
+];
+export const products = [
+    {
+        bannerURL: require('../assets/images/product/product1.jpg'),
+        title: 'Livro Acenda a Sua Luz',
+        info: 'Carol Rache',
+        price: 54.00,
+        ID: 39
+    },
+    {
+        bannerURL: require('../assets/images/product/product2.jpg'),
+        title: 'Camiseta Solidária',
+        info: 'Tamanho único',
+        price: 149.00,
+        ID: 62
+    },
+    {
+        bannerURL: require('../assets/images/product/product3.jpg'),
+        title: 'Tapete Viagem Pássaro',
+        info: '',
+        price: 237.00,
+        ID: 72
+    },
+    {
+        bannerURL: require('../assets/images/product/bg-product1.jpg'), 
+        title: "Condicionador Namah",
+        info: '300 ml',
+        price: 120.00,
+        ID: 404
+    }
+];
 export const posts = [
     {
+        type: 'podcast',
         bannerURL: require('../assets/images/post/bg-post3.jpg'),
         title: "Nome do episódio do podcast lorem ipsum dolor sit amet consectetur eli...",
-        info: ["Namahcast #122"],
+        info: {
+            userNickname: "Namahcast", 
+            userId: "122"
+        },
         ID: 11
     },
     {
+        type: 'post',
         bannerURL: require('../assets/images/post/bg-post2.jpg'),
         title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        info: ["VIAGEM","por Fulano de Tal"],
+        info: {
+            type: "VIAGEM",
+            onwerShip: "Fulano de Tal"
+        },
         article: [
             {
                 type: 'title',
@@ -49,45 +89,22 @@ export const posts = [
         ID: 681
     },
     {
-        bannerURL: require('../assets/images/product/bg-product1.jpg'), 
-        title: "Condicionador Namah",
-        info: ["COMPRE AGORA"],
+        type: 'product',
+        bannerURL: products[filterByID(404,products)].bannerURL,
+        title: products[filterByID(404,products)].title,
         ID: 404
     },
     {
+        type: 'course',
         bannerURL: require('../assets/images/post/bg-post4.jpg'),
         title: "Imersão Vinyasa Flow",
-        info: ["CURSO","04 ABR"],
+        info: {
+            type: "CURSO",
+            date: {
+                day: "04",
+                month: "ABR"
+            }
+        },
         ID: 12023452
-    }
-];
-export const products = [
-    {
-        bannerURL: require('../assets/images/product/product1.jpg'),
-        title: 'Livro Acenda a Sua Luz',
-        info: 'Carol Rache',
-        price: 54.00,
-        ID: 39
-    },
-    {
-        bannerURL: require('../assets/images/product/product2.jpg'),
-        title: 'Camiseta Solidária',
-        info: 'Tamanho único',
-        price: 149.00,
-        ID: 62
-    },
-    {
-        bannerURL: require('../assets/images/product/product3.jpg'),
-        title: 'Tapete Viagem Pássaro',
-        info: '',
-        price: 237.00,
-        ID: 72
-    },
-    {
-        bannerURL: require('../assets/images/product/bg-product1.jpg'), 
-        title: "Condicionador Namah",
-        info: '300 ml',
-        price: 120.00,
-        ID: 404
     }
 ];
