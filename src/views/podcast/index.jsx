a413c7f78e42f6de31ea08649e5a4366f4900606
pepeyen <erickfcaldeira@gmail.com>
2020-10-03 16:13:51 -0300

    Introduced podcast page

---
+++ src/views/podcast/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+//Component
+import Navbar from '../../components/Navbar';
+import Post from '../../components/Post';
+
+//Services
+import {posts} from '../../services/mockData';
+import {getCurrentPageID} from '../../services/getCurrentPage';
+import filterByID from '../../services/filterByID';
+
+function Podcast(){
+    const podcastID = filterByID(getCurrentPageID(),posts);
+
+    return(
+        <React.Fragment>
+            <header>
+                <Navbar />
+            </header>
+            <main>
+                <section className="page">
+                    <Post>
+                        <img 
+                            className="page__post-banner"
+                            src={posts[podcastID].bannerURL} 
+                            alt={posts[podcastID].title}  
+                        />
+                        <p className="page__post-title --centralized-text">{posts[podcastID].title}</p>
+                        <div className="page__post-info">
+                                <span className="page__post-onwership --grey-text --bottom-thin-borders">{posts[podcastID].info.userNickname} #{posts[podcastID].info.userId}</span>
+                        </div>
+                    </Post>
+                </section>
+            </main>
+        </React.Fragment>
+    );
+}
+
+export default Podcast;
