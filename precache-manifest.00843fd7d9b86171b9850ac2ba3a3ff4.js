cbe590e3d19295d4bfd4d6b65df7aa0bf2cc7ed1
pepeyen <erickfcaldeira@gmail.com>
2020-10-05 14:02:43 -0300

    Updates

---
+++ precache-manifest.00843fd7d9b86171b9850ac2ba3a3ff4.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a3e5d58188e37d88805481281cfe53",
+    "url": "/big-bang-web/index.html"
+  },
+  {
+    "revision": "30814bd20c1d3667307a",
+    "url": "/big-bang-web/static/css/main.7913e0c1.chunk.css"
+  },
+  {
+    "revision": "d13ddc0b7de47eedd13f",
+    "url": "/big-bang-web/static/js/2.99434aab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/big-bang-web/static/js/2.99434aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30814bd20c1d3667307a",
+    "url": "/big-bang-web/static/js/main.90d31850.chunk.js"
+  },
+  {
+    "revision": "8255e6fc33e2f18e4107",
+    "url": "/big-bang-web/static/js/runtime-main.6ba7aec4.js"
+  },
+  {
+    "revision": "1cf9c73216a3e6ce8354f8ae88756d20",
+    "url": "/big-bang-web/static/media/Adamina-Regular.1cf9c732.eot"
+  },
+  {
+    "revision": "32e31ea6c8f38bce55b5c377cb66d84d",
+    "url": "/big-bang-web/static/media/Adamina-Regular.32e31ea6.woff"
+  },
+  {
+    "revision": "702b0d7edc9f00c49d11c6c9753fc5d6",
+    "url": "/big-bang-web/static/media/Adamina-Regular.702b0d7e.svg"
+  },
+  {
+    "revision": "ce0435ddfd9fc5efcac92ad41ce93be8",
+    "url": "/big-bang-web/static/media/Adamina-Regular.ce0435dd.ttf"
+  },
+  {
+    "revision": "1b04f6bf96852188b6de37ba12b01ab1",
+    "url": "/big-bang-web/static/media/Chloe-Regular.1b04f6bf.svg"
+  },
+  {
+    "revision": "328d3e21183998a1cdda3372123f6d70",
+    "url": "/big-bang-web/static/media/Chloe-Regular.328d3e21.woff"
+  },
+  {
+    "revision": "3f75a25d28995302e73fb0490e79b35d",
+    "url": "/big-bang-web/static/media/Chloe-Regular.3f75a25d.eot"
+  },
+  {
+    "revision": "e505f205cc5a645580b2dbf27c1e3874",
+    "url": "/big-bang-web/static/media/Chloe-Regular.e505f205.ttf"
+  },
+  {
+    "revision": "0229f54a4e5cfdbd3cfdacbe86e99e0c",
+    "url": "/big-bang-web/static/media/ProximaNova-Bold.0229f54a.svg"
+  },
+  {
+    "revision": "21bae7f72b17ee2d81162136a1c9cdf4",
+    "url": "/big-bang-web/static/media/ProximaNova-Bold.21bae7f7.ttf"
+  },
+  {
+    "revision": "3b70f0d9979f78e3780fd77269c3fe9f",
+    "url": "/big-bang-web/static/media/ProximaNova-Bold.3b70f0d9.woff"
+  },
+  {
+    "revision": "caa7d8653555e3c3d30a5c860acea36d",
+    "url": "/big-bang-web/static/media/ProximaNova-Bold.caa7d865.eot"
+  },
+  {
+    "revision": "6d279ef230b5de952fbbb952ddfd98aa",
+    "url": "/big-bang-web/static/media/ProximaNova-Regular.6d279ef2.svg"
+  },
+  {
+    "revision": "7e564d19e8fbcf7170540582fc5a1c9f",
+    "url": "/big-bang-web/static/media/ProximaNova-Regular.7e564d19.ttf"
+  },
+  {
+    "revision": "e439d2f5096644b34258bd8be9f4ed8f",
+    "url": "/big-bang-web/static/media/ProximaNova-Regular.e439d2f5.woff"
+  },
+  {
+    "revision": "f3d966fb4879b9448d0828e6b16a1025",
+    "url": "/big-bang-web/static/media/ProximaNova-Regular.f3d966fb.eot"
+  },
+  {
+    "revision": "53b53a1a120d34a769aa59c5def113a0",
+    "url": "/big-bang-web/static/media/ProximaNova-Semibold.53b53a1a.eot"
+  },
+  {
+    "revision": "7c17940a9a40805f233609a2f0c78080",
+    "url": "/big-bang-web/static/media/ProximaNova-Semibold.7c17940a.svg"
+  },
+  {
+    "revision": "f83281a7796841966c252267f3b90827",
+    "url": "/big-bang-web/static/media/ProximaNova-Semibold.f83281a7.ttf"
+  },
+  {
+    "revision": "fc0c9d86159b38851d673ecd8e0077da",
+    "url": "/big-bang-web/static/media/ProximaNova-Semibold.fc0c9d86.woff"
+  },
+  {
+    "revision": "2896ed4b0bed3f86f7bc55539cb074c0",
+    "url": "/big-bang-web/static/media/bg-post1.2896ed4b.jpg"
+  },
+  {
+    "revision": "4393fc1b76711ce0b23f1c9dff990e33",
+    "url": "/big-bang-web/static/media/bg-post2.4393fc1b.jpg"
+  },
+  {
+    "revision": "12132c397698ecba1e28ffb232da6046",
+    "url": "/big-bang-web/static/media/bg-post3.12132c39.jpg"
+  },
+  {
+    "revision": "c71c673a124c815ec7d2e97594eb1e94",
+    "url": "/big-bang-web/static/media/bg-post4.c71c673a.jpg"
+  },
+  {
+    "revision": "bcf56719f2fe80bb78babb5074a5d33e",
+    "url": "/big-bang-web/static/media/bg-product1.bcf56719.jpg"
+  },
+  {
+    "revision": "5326b52a51b898e5a83681e23ff80751",
+    "url": "/big-bang-web/static/media/button-foward.5326b52a.svg"
+  },
+  {
+    "revision": "dc5cbf4985b60f7c39e482ae9bd02a98",
+    "url": "/big-bang-web/static/media/button-play.dc5cbf49.svg"
+  },
+  {
+    "revision": "6c0f039bfbfbd4c60640c63634e920a0",
+    "url": "/big-bang-web/static/media/icon-cart.6c0f039b.svg"
+  },
+  {
+    "revision": "970802d2e4b7c3994d40399b8dd38f8a",
+    "url": "/big-bang-web/static/media/icon-logout.970802d2.svg"
+  },
+  {
+    "revision": "df334acbd098e74596e1e7e006d5f09a",
+    "url": "/big-bang-web/static/media/icon-orders.df334acb.svg"
+  },
+  {
+    "revision": "3ed6519adca6d40b3ebab7be8bf94a39",
+    "url": "/big-bang-web/static/media/icon-profile.3ed6519a.svg"
+  },
+  {
+    "revision": "75a92584741765c6b7364d7d2d6879b6",
+    "url": "/big-bang-web/static/media/icon-search.75a92584.svg"
+  },
+  {
+    "revision": "dc611088154603d2ff1f49f6d41b09e9",
+    "url": "/big-bang-web/static/media/icon-wishes.dc611088.svg"
+  },
+  {
+    "revision": "20e860b6855a66bace3b49e265b5f58a",
+    "url": "/big-bang-web/static/media/logo.20e860b6.svg"
+  },
+  {
+    "revision": "2b5ca432c9be346226f8a1dbfbf20008",
+    "url": "/big-bang-web/static/media/podcast-theme.2b5ca432.mp3"
+  },
+  {
+    "revision": "ca1f830fada2622228a0f6f06b456328",
+    "url": "/big-bang-web/static/media/product1.ca1f830f.jpg"
+  },
+  {
+    "revision": "bc0876fb4d81297587f8172a654a1bfa",
+    "url": "/big-bang-web/static/media/product2.bc0876fb.jpg"
+  },
+  {
+    "revision": "01eb1f835f09f7963409af3e9d74bbd8",
+    "url": "/big-bang-web/static/media/product3.01eb1f83.jpg"
+  }
+]);
