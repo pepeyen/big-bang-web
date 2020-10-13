78ca3b3e126a9bc838e0db56f4e102691f36f87d
pepeyen <erickfcaldeira@gmail.com>
2020-10-12 21:59:53 -0300

    Updated button when there's not an active target

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,15 +5,24 @@ import { Link } from 'react-router-dom';
 import img_foward from '../../assets/images/button/button-foward.svg';
 
 function Button(props){
-    return(
-        <Link 
-            className={`page__button --centralized-text --light-text --root-${props.root}`}
-            to={props.target === undefined ? '/#' : `/${props.target}`}
-        > 
-            <span>{props.text.toUpperCase()}</span>
-            {props.root !== 'post' ? '' : <img className="page__button-foward" src={img_foward} alt="Foward" />}
-        </Link>
-    );
+    if(props.target === undefined){
+        return(
+            <div className={`page__button --centralized-text --light-text --root-${props.root}`}> 
+                <span>{props.text.toUpperCase()}</span>
+                {props.root !== 'post' ? '' : <img className="page__button-foward" src={img_foward} alt="Foward" />}
+            </div>
+        );
+    }else{
+        return(
+            <Link 
+                className={`page__button --centralized-text --light-text --root-${props.root}`}
+                to={`/${props.target}`}
+            > 
+                <span>{props.text.toUpperCase()}</span>
+                {props.root !== 'post' ? '' : <img className="page__button-foward" src={img_foward} alt="Foward" />}
+            </Link>
+        );
+    }
 }
 
 export default Button;
