249b7b06272ff4b3077c953ac8185f4924de94a6
pepeyen <erickfcaldeira@gmail.com>
2020-10-14 13:39:41 -0300

    Introduced newsletter component

---
+++ src/components/Newsletter/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function NewsLetter(){
+    console.log('brun')
+    return(
+        <div className="newsletter">
+            <span className="newsletter__title --dark-text">Namah no seu e-mail</span>
+            <span className="newsletter__description --grey-text">Assine nossa newsletter</span>
+            <form className="newsletter__form">
+                <input 
+                    className="newsletter__form-email --thin-borders"
+                    type="email"
+                    placeholder="Seu e-mail"
+                />
+                <input 
+                    className="newsletter__form-button --dark-text --medium-borders --grey-borders"
+                    type="button"
+                    value="ASSINAR"
+                />
+            </form>
+            <div className="newsletter__info --grey-text">
+                <span>Ao clicar em “assinar”, você concorda em receber e-mails do Espaço Namah a aceita nossos <span className="--underlined">Termos de Uso</span> e nossas <span className="--underlined">Políticas de Privacidade</span>.</span>
+            </div>
+        </div>
+    );
+}
+
+export default NewsLetter;
