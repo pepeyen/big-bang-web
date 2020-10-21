37206c63600b12bb7bf5bf96e702d40a1241c7b6
pepeyen <erickfcaldeira@gmail.com>
2020-10-21 16:32:06 -0300

    Minor LikeButton quality of life update

--- src/reducers/likeButtonReducer.js
+++ src/reducers/likeButtonReducer.js
@@ -1,6 +1,6 @@
 import {findInArray,filterInArray} from '../services/findInArray';
 
-const likeButtonReducer = (state = [], action) => {
+const likeButtonReducer = (state = JSON.parse(window.sessionStorage.getItem("likedItems")) === null ? [] : JSON.parse(window.sessionStorage.getItem("likedItems")), action) => { 
     switch(action.type){
         case 'LIKE_A_ITEM':
             let likedList = state;
