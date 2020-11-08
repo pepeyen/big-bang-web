b28c078ef127dc30f1714a0af80aef5deab6e49c
pepeyen <erickfcaldeira@gmail.com>
2020-11-08 15:42:10 -0300

    Dropped mock up API from the Podcast page

--- src/views/Podcast.jsx
+++ src/views/Podcast.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import {Redirect} from 'react-router-dom';
+import React, {
+    useEffect,
+    useState
+}from 'react';
+import {
+    Link,
+    Redirect
+} from 'react-router-dom';
 
 //Component
 import {
@@ -10,38 +16,54 @@ import {
 } from '../components';
 
 //Services
-import {API} from '../services/mockData';
-import {
-    getCurrentPageID,
-    getCurrentPageType,
-    filterOverall
-} from '../services';
+import {getCurrentPageID} from '../services';
 
 export const Podcast = () => {
-    const podcast = filterOverall(getCurrentPageID(),'podcast',API);
+    const [podcast,setPodcast] = useState('');
+    const [isLoading,setIsLoading] = useState(true);
 
-    if(podcast !== -1 && podcast === filterOverall(getCurrentPageID(),getCurrentPageType(),API)){
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_BACK_END_HOST}/api/podcasts/${getCurrentPageID()}`, {
+            method: 'GET'
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            if(data.success !== true){
+                setPodcast(-1);
+            }else{
+                setIsLoading(false);
+                setPodcast(data.podcasts);
+            }
+        })
+    },[]);
+
+    if(podcast !== -1 && getCurrentPageID() !== null){
         return(
             <React.Fragment>
                 <header>
                     <Navbar />
                 </header>
                 <main>
-                    <section className="page">
+                    <section className={isLoading ? "page --loading" : "page"}>
                         <Post>
                             <img 
                                 className="page__post-banner"
-                                src={podcast.media.bannerURL} 
-                                alt={podcast.info.name}  
+                                src={`${process.env.REACT_APP_BLOB_HOST}/jpeg/podcast/bg-${getCurrentPageID()}.jpg`} 
+                                alt={podcast.podcast_title ? podcast.podcast_title : 'Loading'}  
                             />
                             <div className="page__podcast --fade-up">
                                 <AudioPlayer>
-                                    <AudioPlayerButton audioURL={podcast.media.audioURL} />
+                                    <AudioPlayerButton audioURL={`${process.env.REACT_APP_BLOB_HOST}/mp3/podcasts/media-${getCurrentPageID()}.mp3`} />
                                 </AudioPlayer>      
                             </div>
-                            <p className="page__post-title --centralized-text">{podcast.info.podcastName}</p>
+                            <p className="page__post-title --centralized-text">{podcast.podcast_title ? podcast.podcast_title : 'Loading'}</p>
                             <div className="page__post-info">
-                                    <span className="page__post-onwership --grey-text --bottom-thin-borders">{podcast.info.onwership.username} #{podcast.info.onwership.userID}</span>
+                                    <Link 
+                                        to={`/user/${podcast.podcast_author}`}
+                                        className="page__post-onwership --grey-text --bottom-thin-borders">{podcast.podcast_author ? podcast.podcast_author : 'Loading'} #{getCurrentPageID()}
+                                    </Link>
                             </div>
                         </Post>
                     </section>
