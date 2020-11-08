a357f61266bec722a419f03b573ad1aecad0a41e
pepeyen <erickfcaldeira@gmail.com>
2020-11-08 02:00:54 -0300

    Dopped mock up API from the Product page

--- src/views/Product.jsx
+++ src/views/Product.jsx
@@ -1,38 +1,54 @@
-import React from 'react';
+import React, {
+    useEffect,
+    useState
+} from 'react';
 import {Redirect} from 'react-router-dom';
 
 //Components
 import {Navbar} from '../components';
 
 //Services
-import {API} from '../services/mockData';
-import {
-    getCurrentPageID,
-    getCurrentPageType,
-    filterOverall
-} from '../services';
+import {getCurrentPageID} from '../services';
 
 const Product = () => {
-    const product = filterOverall(getCurrentPageID(),'product',API);
+    const [product,setProduct] = useState('');
+    const [isLoading,setIsLoading] = useState(true);
 
-    if(product !== -1 && product === filterOverall(getCurrentPageID(),getCurrentPageType(),API)){
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_BACK_END_HOST}/api/products/${getCurrentPageID()}`, {
+            method: 'GET'
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            if(data.success === false){
+                setProduct(-1);
+            }else{
+                setIsLoading(false);
+                setProduct(data.products);
+            }
+        })
+    },[]);
+
+    if(product !== -1 && getCurrentPageID() !== null){
         return(
             <React.Fragment>
                 <header>
                     <Navbar />
                 </header>
                 <main>
-                    <section className="page">
+                    <section className={isLoading ? "page --loading" : "page"}>
                         <div className="page__product">
                             <img 
                                 className="page__product-image"
-                                src={product.media.bannerURL} 
-                                alt={product.info.title} 
+                                src={`${process.env.REACT_APP_BLOB_HOST}/jpeg/product/bg-${getCurrentPageID()}.jpg`} 
+                                alt={product.product_name ? product.product_name : 'Loading'} 
                             />
                             <div className="page__product-info">
-                                <p className="page__filler-product-name">{product.info.title}</p>
-                                <p className="page__filler-product-info">{product.info.description}</p>
-                                <p className="page__filler-product-price">R$ {product.info.price.toFixed(2)}</p>
+                                <p className="page__filler-product-name">{product.product_name ? product.product_name : 'Loading'}</p>
+                                <p className="page__filler-product-info">{product.product_description ? product.product_description : 'Loading'}</p>
+                                <p className="page__filler-product-price">R$ {product.product_price ? product.product_price.toFixed(2) : 0}</p>
                             </div>
                         </div>
                     </section>
