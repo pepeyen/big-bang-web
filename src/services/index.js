5b3bcb32ac82bdde3e2fb5cc9f90a27bff363f00
pepeyen <erickfcaldeira@gmail.com>
2020-11-23 00:16:41 -0300

    Introduced second to minutes converter service

--- src/services/index.js
+++ src/services/index.js
@@ -148,3 +148,10 @@ export const fetchFromBackEnd = (fetchinEndPoint,fetchingParam,fetchingOptions) 
         })
     });
 };
+
+export const secondsToMinutes = (valueInSeconds) => {
+    const minutes = Math.floor(valueInSeconds / 60);
+    const seconds = Math.floor(valueInSeconds - (minutes * 60));
+
+    return (minutes < 10 ? `0${minutes}` : minutes) + ":" + (seconds < 10 ? `0${seconds}`: seconds);
+};
