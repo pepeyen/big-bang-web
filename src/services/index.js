47d423ce480da2471003d5290e869800c9a152e3
pepeyen <erickfcaldeira@gmail.com>
2020-11-23 13:34:30 -0300

    Introduced a service to get the muse position when it's clicked inside a given element

--- src/services/index.js
+++ src/services/index.js
@@ -155,3 +155,20 @@ export const secondsToMinutes = (valueInSeconds) => {
 
     return (minutes < 10 ? `0${minutes}` : minutes) + ":" + (seconds < 10 ? `0${seconds}`: seconds);
 };
+
+export const getMouseClickPosition = (event, axys, componentId) => {
+    const component = document.getElementById(componentId);
+    let clickedPosition, componentFullSize;
+
+    if(axys === 'x'){
+        clickedPosition = event.pageX - component.offsetLeft;
+        componentFullSize = component.offsetWidth;
+    }else{
+        if(axys === 'y'){
+            clickedPosition = event.pageY - component.offsetTop;
+            componentFullSize = component.offsetHeight;
+        }
+    }
+
+    return  clickedPosition / componentFullSize;  
+};
