ea8a10fe906fa589befefc4412717b495283b2e1
pepeyen <erickfcaldeira@gmail.com>
2020-11-25 12:43:17 -0300

    Introduced new Post display property

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,7 @@ const Post = (props) => {
     let listingType = props.listing === 'table' ? props.listing : null;
 
     return(
-        <article className={`page__post --central ${props.title !== undefined ? '--top-thin-borders --spaced-top' : ''} ${listingType !== null ? `--${listingType}` : ''}`}>
+        <article className={`page__post ${props.full ? '--full' : '--central'} ${props.title !== undefined ? '--top-thin-borders --spaced-top' : ''} ${listingType !== null ? `--${listingType}` : ''}`}>
             {props.title !== undefined ? <span className="page__post-title --dark-text --negative-top">{props.title}</span> : ''}
             {props.children}
         </article>
