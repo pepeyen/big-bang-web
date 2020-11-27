1cbef47ef825ad40c1f72182e02c2378c0a683dc
pepeyen <erickfcaldeira@gmail.com>
2020-11-27 12:54:46 -0300

    Introduced Wishlist view

---
+++ src/views/Wishlist.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+
+//Components
+import {
+    TableListing,
+    ProductCard,
+    Page,
+    Post
+} from '../components';
+
+const WishList = () => {
+    const likedItems = useSelector(state => state.likedItems);
+
+    if(likedItems.products.likedItemsList.length === 0){
+        return(
+            <Page pageTitle="Wishlist">
+                <Post>
+                    <span className="page__feedback">Your wishlist is empty</span>
+                </Post>
+            </Page>
+        );
+    }else{
+        return(
+            <Page pageTitle="Wishlist">
+                <Post>
+                    <TableListing listingType="four-desktop">
+                        {
+                            likedItems.products.likedItemsList.map((element, index) => {
+                                return(
+                                    <ProductCard
+                                        key={index}
+                                        productID={element}
+                                    />
+                                );
+                            })
+                        }
+                    </TableListing>
+                </Post>
+            </Page>
+        );
+    }
+};
+
+export default WishList;
